2dfd8320d7a470ad40101e5b4552ff673d17ca07
Dan Ventura <danice.ventura@gmail.com>
2022-04-15 15:04:25 -0600

    fixed feed render with streetform

--- src/pages/StreetFeed.js
+++ src/pages/StreetFeed.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 //import { Link } from 'react-router-dom'
 import StreetPost from '../components/StreetPost'
+import StreetForm from '../components/StreetForm'
 
 const StreetFeed = () => {
   const [allStreets, setAllStreets] = useState([])
@@ -17,6 +18,9 @@ const StreetFeed = () => {
 
   return (
     <div>
+      <div>
+        <StreetForm />
+      </div>
       <div className="allStreetGrid">
         {allStreets.map((street) => (
           <StreetPost authorId={street.authorId} content={street.content} />
