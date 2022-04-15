e99e5d6a95837eca15eeb92a1825df46388778a8
Dan Ventura <danice.ventura@gmail.com>
2022-04-15 12:25:30 -0600

    added street form comp and street post comp, a few css changes to nav

--- src/components/StreetForm.js
+++ src/components/StreetForm.js
@@ -1,1 +1,42 @@
-hold
+import { useState } from 'react'
+import axios from 'axios'
+//import { useNavigate } from 'react-router-dom'
+
+const StreetForm = () => {
+  // let navigate = useNavigate()
+  const [streetValues, setStreetValues] = useState({
+    content: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // axios
+    //  .post(`http://localhost:3001/user/new`, streetValues)
+    // .then((res) => console.log('successful'))
+    // .catch((err) => console.log(err.data))
+    setStreetValues({ content: '' })
+  }
+
+  const handleChange = (e) => {
+    setStreetValues({ ...streetValues, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <div className="streetFormComp">
+      <h2>Create New Street</h2>
+      <form onSubmit={handleSubmit} className="streetForm">
+        <input
+          required
+          type="text"
+          name="Content"
+          value={streetValues.content}
+          placeholder="Content"
+          onChange={handleChange}
+        ></input>
+        <button disabled={!streetValues.content}>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default StreetForm
--- src/components/StreetPost.js
+++ src/components/StreetPost.js
@@ -1,1 +1,13 @@
-hold
+//import { useState } from 'react'
+//import axios from 'axios'
+//import { useNavigate } from 'react-router-dom'
+
+const StreetPost = (props) => {
+  return (
+    <div>
+      <h3>Street</h3>
+    </div>
+  )
+}
+
+export default StreetPost
