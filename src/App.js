6bae1410c097f91fa0a6e5e7ac61cce38deb0993
Dan Ventura <danice.ventura@gmail.com>
2022-04-15 09:14:57 -0600

    added comp files, page files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './styles/App.css'
+import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Filler</header>
+      <header className="App-header">
+        <NavBar />
+      </header>
+      <main>{/* <Routes><Route /></Routes> */}</main>
     </div>
   )
 }
