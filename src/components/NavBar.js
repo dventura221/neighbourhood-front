ccfdfa19714d3ca60b8c4e33ef8a8dc4446a107c
Dan Ventura <danice.ventura@gmail.com>
2022-04-15 10:44:00 -0600

    added sign in component/page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,14 @@ const NavBar = () => {
   return (
     <nav>
       <div className="navLinkBar">
-        {/* <Link className="navLink">Home</Link>
-        <Link className="navLink">Your Streets</Link>
+        {/* <Link className="navLink" to="/register">
+          Register
+        </Link> */}
+        {/* <Link className="navLink">Home</Link> */}
+        {/* <Link className="navLink" to="/signin">
+          Sign In
+        </Link> */}
+        {/* <Link className="navLink">Your Streets</Link>
         <Link className="navLink">Your Comments</Link> */}
       </div>
       <h1 className="pageTitle">The NeighboUrhood</h1>
