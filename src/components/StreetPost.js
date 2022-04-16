e49014adfb9d40f711dc31dafcacc34dfb74f4c4
Dan Ventura <danice.ventura@gmail.com>
2022-04-16 11:41:56 -0600

    comments now render below street, but need to display only comments related to that street

--- src/components/StreetPost.js
+++ src/components/StreetPost.js
@@ -1,21 +1,41 @@
-//import { useState } from 'react'
-//import axios from 'axios'
-//import { useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+//import { useParams } from 'react-router-dom'
 import CommentForm from './CommentForm'
 import Comment from './Comment'
 
 const StreetPost = (props) => {
+  //let { id } = useParams()
+  const [allComments, setAllComments] = useState([])
+
+  useEffect(() => {
+    const getComments = async () => {
+      const results = await axios.get(`http://localhost:3001/comment/1`)
+      setAllComments(results.data)
+    }
+    getComments()
+  }, [])
+
   return (
     <div>
       <div>
         <h3>Street {props.id}</h3>
         <h4>Author: {props.authorId}</h4>
-        <p>Content: {props.content}</p>
+        <p>Street Content: {props.content}</p>
       </div>
       <div>
         <CommentForm />
-        <Comment streetId={props.id} authorId={props.authorId} />
-        {/* Add .map to show multiple comments, conditional render the multiple
+        <div className="allCommentsGrid">
+          {allComments.map((comment) => (
+            <Comment
+              key={comment.id}
+              streetId={comment.id}
+              authorId={comment.authorId}
+              content={comment.content}
+            />
+          ))}
+        </div>
+        {/* Add conditional render the multiple
         comments. */}
       </div>
     </div>
