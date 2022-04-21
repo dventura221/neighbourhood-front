c10b2e905aab0640ed820ff4c9865f7567ca62f0
Dan Ventura <danice.ventura@gmail.com>
2022-04-21 09:13:07 -0600

    changed BASE_URL to heroku deployed back-end

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+//export const BASE_URL = 'http://localhost:3001'
+//local REACT App
+
+export const BASE_URL = 'https://neighbourhoodbackend.herokuapp.com'
+// Heroku URL
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
