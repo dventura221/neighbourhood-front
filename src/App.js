a57470b68119862c1645756fd94ec013cd322220
Dan Ventura <danice.ventura@gmail.com>
2022-04-21 12:22:31 -0600

    edit profile no longer crashes

--- src/App.js
+++ src/App.js
@@ -44,7 +44,9 @@ export default function App() {
               />
             }
           />
-          <Route path="/profile" element={<UpdateUser user={user} />} />
+          {user && (
+            <Route path="/profile" element={<UpdateUser user={user} />} />
+          )}
         </Routes>
       </main>
     </div>
