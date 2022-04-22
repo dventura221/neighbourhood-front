e94d2327c6dc776ff8e6099f10f5e770f66a8a20
Dan Ventura <danice.ventura@gmail.com>
2022-04-21 22:02:56 -0600

    location and zip search added to weathercard

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -4,20 +4,34 @@ import { wAPI } from '../globals'
 
 const WeatherCard = () => {
   const [weather, setWeather] = useState()
+  const [zip, setZip] = useState(80236)
+  const [zipForm, setZipForm] = useState('')
 
   useEffect(() => {
     let getWeather = async () => {
       const weatherResults = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${wAPI}&units=imperial`
+        `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${wAPI}&units=imperial`
       )
       setWeather(weatherResults.data)
     }
     getWeather()
-  }, [])
+  }, [zip])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setZip(zipForm)
+    setZipForm('')
+  }
 
   return weather !== undefined ? (
     <div>
       <h3>Pern Place Weather</h3>
+      <h5>
+        Location: {weather.name},&nbsp;
+        {new Intl.DisplayNames(['en'], { type: 'region' }).of(
+          weather.sys.country
+        )}
+      </h5>
       <h4>{weather.weather[0].main}</h4>
       <h4>Detail: {weather.weather[0].description}</h4>
       <img
@@ -30,6 +44,19 @@ const WeatherCard = () => {
       <p>Wind Speed: {weather.wind.speed} mph</p>
       <p>Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleString()}</p>
       <p>Sunset: {new Date(weather.sys.sunset * 1000).toLocaleString()}</p>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            type="number"
+            name="zip"
+            value={zipForm}
+            placeholder="Weather by Zipcode"
+            onChange={(e) => setZipForm(e.target.value)}
+          ></input>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   ) : null
 }
