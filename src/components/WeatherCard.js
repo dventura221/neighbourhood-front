a00fae85b3d3be4a3598dc5123c68a8b2f1f18a1
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-04-22 09:09:48 -0400

    Weather Mod Final Formats

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -30,27 +30,6 @@ const WeatherCard = () => {
         <h1>PERN</h1>
         <p id="Weather">Weather</p>
         <img src="https://imgur.com/sMFwEWm.jpg" />
-        <h4>Current Forecast:</h4>
-        <h2>{weather.weather[0].main}</h2>
-        {/* <h4>Detail: {weather.weather[0].description}</h4> */}
-        <div className="OpenWeather">
-          <img
-            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-            alt="Icon"
-          />
-        </div>
-        <p>Temp: {weather.main.temp} F</p>
-        <p>Feels Like: {weather.main.feels_like} F</p>
-        <p>Humidity: {weather.main.humidity}%</p>
-        <p>Wind Speed: {weather.wind.speed} mph</p>
-        <p className="Sun">
-          Sunrise: <br />
-          {new Date(weather.sys.sunset * 1000).toLocaleString()}
-        </p>
-        <p className="Sun">
-          Sunset: <br />
-          {new Date(weather.sys.sunrise * 1000).toLocaleString()}
-        </p>
         <h5>
           Location: {weather.name},&nbsp;
           {new Intl.DisplayNames(['en'], { type: 'region' }).of(
@@ -58,7 +37,39 @@ const WeatherCard = () => {
           )}
           &nbsp;{zip}
         </h5>
-        <div>
+        <h4>Current Forecast:</h4>
+        <h2 id="Forecast">{weather.weather[0].main}</h2>
+        {/* <h4>Detail: {weather.weather[0].description}</h4> */}
+        <div className="OpenWeather">
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt="Icon"
+          />
+        </div>
+        <div className="Details">
+          <p>
+            <span>Temp:</span> {weather.main.temp} F
+          </p>
+          <p>
+            <span>Feels Like:</span> {weather.main.feels_like} F
+          </p>
+          <p>
+            <span>Humidity:</span> {weather.main.humidity}%
+          </p>
+          <p>
+            <span>Wind Speed:</span> {weather.wind.speed} mph
+          </p>
+        </div>
+        <p>
+          <span className="Sun">Sunrise:</span> <br />
+          {new Date(weather.sys.sunrise * 1000).toLocaleString()}
+        </p>
+        <p>
+          <span className="Sun">Sunset: </span>
+          <br />
+          {new Date(weather.sys.sunset * 1000).toLocaleString()}
+        </p>
+        <div className="WeatherSearch">
           <form onSubmit={handleSubmit}>
             <input
               required
